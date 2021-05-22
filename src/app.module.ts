f8a90f053f885285bf4eea38818ca7e07e1042d6
kain <zhangtqx@vip.qq.com>
2021-05-22 14:40:41 +0800

    update config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('database'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [
     AppController,
   ],
