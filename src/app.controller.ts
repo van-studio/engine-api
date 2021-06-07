0ffe43445fee14173fe118f2ae958120fea99fbb
kain <zhangtqx@vip.qq.com>
2021-06-07 20:52:19 +0800

    recover

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,5 @@
-import { BadRequestException, Body, Controller, Get, Post, Put } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users/users.service';
-import { AuthBody } from './types';
-import { argon2id, verify } from 'argon2';
 
 @Controller()
 export class AppController {
@@ -15,37 +13,5 @@ export class AppController {
     return {
       msg: 'hi',
     };
-  }
-
-  @Post('login')
-  async auth(@Body() body: AuthBody) {
-    const data = await this.users.findOne(body.email);
-    if (!data) {
-      throw new BadRequestException(['The email does not exist or password is incorrect']);
-    }
-    const check = await verify(data.password, body.password, {
-      type: argon2id,
-    });
-    if (!check) {
-      throw new BadRequestException(['The email does not exist or password is incorrect']);
-    }
-    // TODO: Factory Auth
-    return {
-      message: ['ok'],
-    };
-  }
-
-  @Post('refresh_token')
-  async refreshToken() {
-
-  }
-
-  @Put('update')
-  async update() {
-  }
-
-  @Put('email')
-  async email() {
-
   }
 }
