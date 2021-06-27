fedebe69ef7e52ee03df1ead2f12fb2011ba1f06
kain <zhangtqx@vip.qq.com>
2021-06-27 21:28:00 +0800

    update

--- model/user.go
+++ model/user.go
@@ -1,11 +1,13 @@
 package model
 
+import "time"
+
 type User struct {
-	ID         uint   `json:"id"`
-	Email      string `gorm:"type:varchar(255);unique" json:"email"`
-	Password   string `gorm:"type:text" json:"-"`
-	Name       string `gorm:"type:varchar(50)" json:"name"`
-	Status     *bool  `gorm:"default:true" json:"status"`
-	CreateTime int64  `gorm:"autoCreateTime" json:"create_time"`
-	UpdateTime int64  `gorm:"autoUpdateTime" json:"update_time"`
+	ID         uint      `json:"id"`
+	Email      string    `gorm:"type:varchar(255);unique" json:"email"`
+	Password   string    `gorm:"type:text" json:"-"`
+	Name       string    `gorm:"type:varchar(50)" json:"name"`
+	Status     *bool     `gorm:"default:true" json:"status"`
+	CreateTime time.Time `gorm:"autoCreateTime" json:"create_time"`
+	UpdateTime time.Time `gorm:"autoUpdateTime" json:"update_time"`
 }
