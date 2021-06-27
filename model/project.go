e6d4339d418696a3b7ca6a93242cced91cdd2c2c
kain <zhangtqx@vip.qq.com>
2021-06-28 01:03:41 +0800

    update

---
+++ model/project.go
@@ -0,0 +1,13 @@
+package model
+
+import "time"
+
+type Project struct {
+	ID          uint      `json:"id"`
+	Key         string    `gorm:"varchar(20);unique" json:"key"`
+	Name        string    `gorm:"type:varchar(50)" json:"name"`
+	Description string    `gorm:"type:text" json:"description"`
+	Status      *bool     `gorm:"default:true" json:"status"`
+	CreateTime  time.Time `gorm:"autoCreateTime" json:"create_time"`
+	UpdateTime  time.Time `gorm:"autoUpdateTime" json:"update_time"`
+}
