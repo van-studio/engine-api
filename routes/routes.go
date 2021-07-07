fbf1f4d4404d7f3a488d7f5cd43d6a19f6242c4c
kain <zhangtqx@vip.qq.com>
2021-07-07 20:55:27 +0800

    update

--- routes/routes.go
+++ routes/routes.go
@@ -3,21 +3,26 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	bit "github.com/kainonly/gin-bit"
+	"github.com/kainonly/gin-helper/authx"
 	"github.com/weplanx/api/controller"
 )
 
 func Initialize(
 	r *gin.Engine,
+	auth *authx.Auth,
 	main *controller.Main,
 	users *controller.Users,
 	projects *controller.Projects,
 ) {
 	r.GET("/", bit.Bind(main.Index))
-	r.POST("/login", bit.Bind(main.Login))
-	r.GET("/verify", bit.Bind(main.Verify))
-	r.DELETE("/logout", bit.Bind(main.Logout))
+	r.POST("/auth", bit.Bind(main.Login))
+	r.GET("/auth", bit.Bind(main.Verify))
+	r.DELETE("/auth", bit.Bind(main.Logout))
 
-	rUsers := r.Group("/users")
+	middlewares := []gin.HandlerFunc{
+		authx.Middleware(*auth),
+	}
+	rUsers := r.Group("/users", middlewares...)
 	{
 		rUsers.POST("/originLists", bit.Bind(users.OriginLists))
 		rUsers.POST("/lists", bit.Bind(users.Lists))
@@ -26,7 +31,7 @@ func Initialize(
 		rUsers.POST("/edit", bit.Bind(users.Edit))
 		rUsers.POST("/delete", bit.Bind(users.Delete))
 	}
-	rProjects := r.Group("projects")
+	rProjects := r.Group("projects", middlewares...)
 	{
 		rProjects.POST("/originLists", bit.Bind(projects.OriginLists))
 		rProjects.POST("/lists", bit.Bind(projects.Lists))
